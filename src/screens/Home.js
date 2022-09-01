8cdf1eeff33a974694fce87d1573fea037eef85f
A7abhilash <a7abhilash@gmail.com>
2022-09-01 10:23:12 +0530

    updt: show my vpa on home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {useAuth} from '../contexts/AuthContext';
-import {fonts} from '../styles';
+import {colors, fonts} from '../styles';
 import MyUserGroups from '../components/home/MyUserGroups';
+import {useNavigation} from '@react-navigation/native';
 
 export default function Home() {
   const {user} = useAuth();
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
@@ -14,6 +16,22 @@ export default function Home() {
         <Text style={styles.headerCaption}>
           Split the bill among friends going on vacation/outing.
         </Text>
+        <TouchableOpacity onPress={() => navigation.navigate('UpdateProfile')}>
+          <View style={styles.showUpiID}>
+            <View>
+              <Text style={styles.upiTextLabel}>My UPI ID (VPA):</Text>
+            </View>
+            <View>
+              <Text
+                style={{
+                  ...styles.upiTextValue,
+                  color: user?.vpa ? colors.Primary : colors.Danger,
+                }}>
+                {user?.vpa ? user?.vpa : 'Tap to set'}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
       </View>
       <MyUserGroups />
     </View>
@@ -31,6 +49,7 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 10,
     marginTop: 10,
+    marginBottom: 5,
   },
   headerTitle: {
     fontSize: 23,
@@ -39,6 +58,23 @@ const styles = StyleSheet.create({
   headerCaption: {
     fontSize: 16,
     marginTop: 5,
+    fontFamily: fonts.PoppinsLight,
+  },
+  showUpiID: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 15,
+    backgroundColor: colors.Light,
+    paddingVertical: 5,
+    paddingHorizontal: 8,
+    marginTop: 5,
+  },
+  upiTextLabel: {
+    color: colors.Dark,
+    fontFamily: fonts.PoppinsMedium,
+  },
+  upiTextValue: {
     fontFamily: fonts.PoppinsLight,
   },
 });
